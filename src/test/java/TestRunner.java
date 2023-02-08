a696129a743799d862af022033cb4d0ef5d9701f
NataliiaGD <nataliagordiyenko8@gmail.com>
2023-02-08 21:46:44 +0200

    made changes

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,28 +3,30 @@ import com.hillel.util.ConfigProvider;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestRunner {
-
-
+        public WebDriver driver;
+        public WebElement testing;
+        public WebElement qaAutomation;
+        public WebElement courseName;
+        public WebDriverWait waiter;
     @Test
     public void openQaAutomationPage () {
-        WebDriver driver =  WebDriverFactory.getDriver();
+        driver =  WebDriverFactory.getDriver();
         driver.get(ConfigProvider.BASE_URL);
-        WebElement testing = driver.findElement(
+        testing = driver.findElement(
                 By.xpath("(//a[@class='block-course-cats_link course-cat-bar'])[2]"));
         testing.click();
-        WebElement qaAutomation = driver.findElement(
+        qaAutomation = driver.findElement(
                 By.xpath("(//p[@class='profession-bar_title'])[2]"));
         qaAutomation.click();
-        WebElement courseName = driver.findElement(
+        courseName = driver.findElement(
                 By.xpath("//span[@class='course-descriptor_header-text']"));
-        WebDriverWait waiter = new WebDriverWait(driver,5);
+        waiter = new WebDriverWait(driver,5);
         waiter.until(ExpectedConditions.visibilityOf(courseName));
         Assert.assertEquals(courseName.getText(),"Курс QA Automation");
         driver.quit();
