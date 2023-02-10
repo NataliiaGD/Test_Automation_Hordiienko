5d56d59b229a30ee8dcecc4c2857e46112d01f02
NataliiaGD <nataliagordiyenko8@gmail.com>
2023-02-10 21:09:22 +0200

    made changes

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -2,7 +2,6 @@ import com.hillel.core.driver.WebDriverFactory;
 import com.hillel.util.ConfigProvider;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -10,23 +9,17 @@ import org.testng.annotations.Test;
 
 public class TestRunner {
         public WebDriver driver =  WebDriverFactory.getDriver();
-        public WebElement testing;
-        public WebElement qaAutomation;
-        public WebElement courseName;
         public WebDriverWait waiter = new WebDriverWait(driver,5);
+        public By testing = By.xpath("(//a[@class='block-course-cats_link course-cat-bar'])[2]");
+        public By qaAutomation = By.xpath("(//p[@class='profession-bar_title'])[2]");
+        public By courseName = By.xpath("//span[@class='course-descriptor_header-text']");
     @Test
     public void openQaAutomationPage () {
         driver.get(ConfigProvider.BASE_URL);
-        testing = driver.findElement(
-                By.xpath("(//a[@class='block-course-cats_link course-cat-bar'])[2]"));
-        testing.click();
-        qaAutomation = driver.findElement(
-                By.xpath("(//p[@class='profession-bar_title'])[2]"));
-        qaAutomation.click();
-        courseName = driver.findElement(
-                By.xpath("//span[@class='course-descriptor_header-text']"));
-        waiter.until(ExpectedConditions.visibilityOf(courseName));
-        Assert.assertEquals(courseName.getText(),"Курс QA Automation");
+        driver.findElement(testing).click();
+        driver.findElement(qaAutomation).click();
+        waiter.until(ExpectedConditions.visibilityOf(driver.findElement(courseName)));
+        Assert.assertEquals(driver.findElement(courseName).getText(),"Курс QA Automation");
         driver.quit();
     }
 }
