bfa4974a6995185bdeaf98eb6321906c8d4ae08b
NataliiaGD <nataliagordiyenko8@gmail.com>
2023-02-12 18:02:14 +0200

    added Hometask14 "Page Object"

--- src/main/java/com/hillel/pages/QaAutomationPage.java
+++ src/main/java/com/hillel/pages/QaAutomationPage.java
@@ -2,7 +2,7 @@ package com.hillel.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public abstract class QaAutomationPage {
+public abstract class QaAutomationPage{
     public WebDriver driver;
     public By courseTitle = By.xpath("//span[@class = 'course-descriptor_header-text']");
     public By courseRate = By.xpath("//span[@class = 'course-rating_average']");
