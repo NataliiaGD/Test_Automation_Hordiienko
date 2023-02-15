2e40150d48ca25120bfbbacfeee0fd11085aee4c
NataliiaGD <nataliagordiyenko8@gmail.com>
2023-02-15 20:33:04 +0200

    made changes according to the comments

--- src/main/java/com/hillel/util/Wrapper.java
+++ src/main/java/com/hillel/util/Wrapper.java
@@ -9,18 +9,14 @@ public class Wrapper {
     protected WebDriver driver;
     WebDriverWait wait = new WebDriverWait(driver,5);
 
-    public void clickWait(WebElement element, WebDriver driver){
-        waitUntilElementIsClickable(element,driver);
+    public void clickWait(WebElement element){
+        isClickableWait(element);
         element.click();
     }
     public void checkIfElementDisappeared(WebElement logo, WebElement courseTitle){
         wait.until(ExpectedConditions.invisibilityOf(courseTitle));
     }
     public void isClickableWait(WebElement element){
-        wait.until(ExpectedConditions.elementToBeClickable(element));
-    }
-    public static void waitUntilElementIsClickable(WebElement element, WebDriver driver){
-        WebDriverWait wait = new WebDriverWait(driver,5);
         wait.ignoring(StaleElementReferenceException.class);
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
